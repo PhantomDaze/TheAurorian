52634266ee32dffe1494f0a3e4fcf171ce7d0e54
PhantomDaze <PhantomC261@outlook.com>
2026-08-03 15:27:10 +0800

    Fix silentwood ladder climbing via minecraft:climbable tag

--- src/main/java/shiroroku/theaurorian/DataGen/DataGenBlocksTags.java
+++ src/main/java/shiroroku/theaurorian/DataGen/DataGenBlocksTags.java
@@ -45,6 +45,7 @@ public class DataGenBlocksTags extends TagsProvider<Block> {
         this.tag(BlockTags.MINEABLE_WITH_AXE).add(BlockRegistry.silentwood_slab.get());
         this.tag(BlockTags.MINEABLE_WITH_AXE).add(BlockRegistry.silentwood_stairs.get());
         this.tag(BlockTags.MINEABLE_WITH_AXE).add(BlockRegistry.silentwood_ladder.get());
+        this.tag(BlockTags.CLIMBABLE).add(BlockRegistry.silentwood_ladder.get());
         this.tag(BlockTags.MINEABLE_WITH_AXE).add(BlockRegistry.weeping_willow_log.get());
         this.tag(BlockTags.MINEABLE_WITH_AXE).add(BlockRegistry.weeping_willow_planks.get());
         this.tag(BlockTags.MINEABLE_WITH_AXE).add(BlockRegistry.weeping_willow_stairs.get());
